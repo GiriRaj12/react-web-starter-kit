a07a5a8988e9097a7e9361d0faa396cff96280db
Giri <girirajsekar50@gmail.com>
2020-08-05 13:31:35 +0530

    React Starter Kit : Add File - Readme, Article Story, Error Boundary

--- src/components/Article/Article.stories.js
+++ src/components/Article/Article.stories.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Article from './Article';
 import SampleArticle from './SampleArticle';
-import {withInfo} from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-actions'
 
 
 export default {
-    title: 'Article',
-    decorators: [withInfo],
-  };
+  title: 'Article',
+  decorators: [withInfo],
+};
 
 export const wholeArticle = () => <Article></Article>;
 
 export const singleSampleArticle = () => <SampleArticle></SampleArticle>
-
-export const 
